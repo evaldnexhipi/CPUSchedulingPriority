b7453f995dcf661c1ca26b7ede8d8749c15f7225
Evald Nexhipi <51530364+evaldnexhipi@users.noreply.github.com>
2020-01-05 12:56:56 +0100

    Update ProcessesFrame.java

--- ProcessesFrame.java
+++ ProcessesFrame.java
@@ -2,10 +2,9 @@ import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Vector;
 
 import javax.swing.JButton;
@@ -21,15 +20,15 @@ public class ProcessesFrame extends JFrame implements ActionListener{
 	private JPanel northPanel;
 	private JPanel centerPanel;
 	private JPanel southPanel;
+	
 	private final int FRAME_WIDTH=1000;
 	private final int FRAME_HEIGHT=600;
-	private JLabel processNumberLabel;
+	
 	private JLabel arrivalTimeLabel;
 	private JLabel burstTimeLabel;
 	private JLabel priorityLabel;
 	private JButton runButton;
 	
-	private JTextField processNumberField;
 	private JTextField arrivalTimeField;
 	private JTextField burstTimeField;
 	private JTextField priorityField;
@@ -45,6 +44,7 @@ public class ProcessesFrame extends JFrame implements ActionListener{
 		setTitle("Skedulimi me Prioritet Ndalues");
 		setSize(FRAME_WIDTH,FRAME_HEIGHT);
 		processesList= new ArrayList <Process>();
+		
 		createNorthPanel();
 		createCenterPanel();
 		createSouthPanel();
@@ -55,29 +55,25 @@ public class ProcessesFrame extends JFrame implements ActionListener{
 	
 	private void createNorthPanel() {
 		northPanel = new JPanel();
-		northPanel.setLayout(new GridLayout(2,5));
-		
-		processNumberLabel = new JLabel("Process Number");
+		northPanel.setLayout(new GridLayout(2,4));
+			
 		arrivalTimeLabel = new JLabel ("Process Arival Time");
 		burstTimeLabel = new JLabel ("Process Burst Time");
 		priorityLabel = new JLabel("Process Priority");
 		runButton = new JButton ("Run Algorithm");
 		runButton.addActionListener(this);
 		
-		processNumberField = new JTextField("");
 		arrivalTimeField = new JTextField ("");
 		burstTimeField = new JTextField ("");
 		priorityField = new JTextField("");
 		addButton = new JButton("Add Process");
 		addButton.addActionListener(this);
 		
-		northPanel.add(processNumberLabel);
 		northPanel.add(arrivalTimeLabel);
 		northPanel.add(burstTimeLabel);
 		northPanel.add(priorityLabel);
 		northPanel.add(runButton);
 		
-		northPanel.add(processNumberField);
 		northPanel.add(arrivalTimeField);
 		northPanel.add(burstTimeField);
 		northPanel.add(priorityField);
@@ -92,7 +88,7 @@ public class ProcessesFrame extends JFrame implements ActionListener{
 		columnNames.add("Arrival Time");
 		columnNames.add("Burst Time");
 		columnNames.add("Priority");
-		//String [] columnNames = {"Process Number", "Arrival Time", "Burst Time","Priority"};
+		
 		Vector data = new Vector<Object>();
 		Vector data1 = new Vector <String>();
 		processTable = new JTable(data,columnNames);
@@ -112,19 +108,20 @@ public class ProcessesFrame extends JFrame implements ActionListener{
 
 	@Override
 	public void actionPerformed(ActionEvent event) {
-		// TODO Auto-generated method stub
 		if (event.getSource()==addButton) {
 			DefaultTableModel model = (DefaultTableModel) processTable.getModel();
 			Vector newRow = new Vector <String>();
 			count++;
+			
 			Process p = new Process (count,Integer.parseInt(arrivalTimeField.getText()),Integer.parseInt(burstTimeField.getText()),Integer.parseInt(priorityField.getText()));
 			processesList.add(p);
+			
 			newRow.add(p.getNumber());
 			newRow.add(p.getArrivalTime());
 			newRow.add(p.getBurstTime());
 			newRow.add(p.getPriority());
 			model.addRow(newRow);
-			//model.removeRow(0);
+			
 		}
 		else if (event.getSource()==runButton) {
 			statisticsArray = new ArrayList<Process>();
@@ -149,14 +146,17 @@ public class ProcessesFrame extends JFrame implements ActionListener{
 				
 				System.out.println("Time: "+t+"-"+(t+1)+" , Process: "+currentProcess.getTitle());
 				if (processesList.size()==0) {
+					
 					System.out.println("\n---STATISTICS----");
 					double sumPritja = 0; double sumQendrimi = 0;
 					sortArray(statisticsArray);
+					
 					for (Process pr : statisticsArray) {
 						sumPritja+=pr.getKohaPritjes();
 						sumQendrimi+=pr.getKohaQendrimit();
 						System.out.println(pr.getTitle()+" Koha e pritjes: "+pr.getKohaPritjes()+" | "+"Koha e qendrimit: "+pr.getKohaQendrimit());
 					}
+					
 					double mesPritja = sumPritja / statisticsArray.size();
 					double mesQendrimi = sumQendrimi / statisticsArray.size();
 					System.out.println("Koha mesatare e pritjes: "+mesPritja);
@@ -169,15 +169,15 @@ public class ProcessesFrame extends JFrame implements ActionListener{
 	
 	public int highestPriorityIndex (ArrayList <Process> list,int time) {
 		int index = 0;
-		int iter = -1;
+		int iter = 0;
 		int min = list.get(0).getPriority();
 		
 		for (Process p : list) {
-			iter++;
 			if (p.getPriority()<min && p.getArrivalTime()<=time) {
 				min=p.getPriority();
 				index=iter;
 			}
+			iter++;
 		}
 		return index;
 	}
@@ -192,4 +192,3 @@ public class ProcessesFrame extends JFrame implements ActionListener{
 		}
 	}
 }
-
